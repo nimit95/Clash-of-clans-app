8a3117525e7d904d121ae4f1bb1301fab9db9e13
Nimit Agg <nimitagg95@gmail.com>
2016-12-13 18:02:19 +0530

    feat: Clash Helper completed

---
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/network/GetResponse.java
@@ -0,0 +1,28 @@
+package capstoneproject.androidnanodegree.com.cochelper.network;
+
+import java.io.IOException;
+
+import capstoneproject.androidnanodegree.com.cochelper.utils.Constants;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by Nimit Agg on 12-12-2016.
+ */
+
+public class GetResponse {
+    OkHttpClient client = new OkHttpClient();
+
+    public String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url).header("Authorization","Bearer " + Constants.AUTH_TOKEN)
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
+    }
+}
---
+++ app/src/main/java/capstoneproject/androidnanodegree/com/cochelper/network/YoutubeAPIResponse.java
@@ -0,0 +1,27 @@
+package capstoneproject.androidnanodegree.com.cochelper.network;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by dell on 12/12/2016.
+ */
+
+public class YoutubeAPIResponse {
+    OkHttpClient client = new OkHttpClient();
+
+    public String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        Log.e("i",response.code()+"");
+        return response.body().string();
+    }
+}
